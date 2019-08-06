ca29ea9200d5023565ef8a260e0b73b5c78cdf5c
Aleksandr <Aleksandr.pushkin.e@gmail.com>
2019-08-06 16:22:13 +0300

    My First Commit

---
+++ PSA.Test.VKAuth/Controllers/VKFriendsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkNet.Abstractions;
+using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
+
+namespace PSA.Test.VKAuth.Controllers
+{
+
+    [Authorize]
+    public class VKFriendsController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+
+        private readonly IVkApi _vkApi;
+
+        public VKFriendsController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+        public  IActionResult Index()
+        {
+            var friends =   _vkApi.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            { Count = 5, UserId = Convert.ToInt64( User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value ), Order = FriendsOrder.Random, Fields = ProfileFields.City | ProfileFields.Domain 
+            }, false);
+            
+
+            return View(friends);
+        }
+
+    }
+}
---
+++ PSA.Test.VKAuth/Extension/PrincipalExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using static AspNet.Security.OAuth.Vkontakte.VkontakteAuthenticationConstants;
+
+namespace PSA.Test.VKAuth.Extension
+{
+    public static class PrincipalExtensions
+    {
+      
+            public static string GetFullName(this ClaimsPrincipal user)
+            {
+                var giveName =user.FindFirst(ClaimTypes.GivenName).Value;
+                var lastName = user.FindFirst(ClaimTypes.Surname).Value;
+                return giveName == null && lastName == null? null : string.Format("{0} {1}",giveName,lastName);
+            }
+            public static string GetThumbnail(this ClaimsPrincipal user)
+            {
+                var claim = ((ClaimsIdentity)user.Identity).FindFirst(Claims.ThumbnailUrl);
+                return claim == null ? null : claim.Value;
+            }
+            
+   
+    }
+}
